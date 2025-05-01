71bf3d67af64bee278ea713f0e06f9a65b2338bd
Ani <sharma.animesh@gmail.com>
2025-05-01 12:32:08 +0000

    Pending changes exported from your codespace

---
+++ .ipynb_checkpoints/install deps-checkpoint.R
@@ -0,0 +1,8 @@
+#https://sib-swiss.github.io/intro-bayesian-statistics-training/website/installation_instructions.html
+install.packages("rstan", repos = c('https://stan-dev.r-universe.dev', getOption("repos")))
+install.packages("rmarkdown")
+install.packages("shiny")
+install.packages("tidyverse")
+install.packages("brms")
+#https://paulbuerkner.com/brms/
+
